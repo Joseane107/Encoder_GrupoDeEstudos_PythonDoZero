c6fa22d47bc4e352ab9551dd319bedededbf26bc
Igorpereirag <igorpereira1501@gmail.com>
2024-02-17 16:44:09 -0300

    pequenas alterações

---
+++ decode.py
@@ -0,0 +1,6 @@
+## vamos criar um codificador de texto onde o programa
+## recebe um texto e retorna o texto codificado     
+
+#vamos usar a biblioteca base64 para codificar o texto
+                   
+import base64
